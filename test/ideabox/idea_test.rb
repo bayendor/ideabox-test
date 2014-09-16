1e63db2998d92a7a139e6945b4867c4dcdbe1d52
David Bayendor <dbayendor@gmail.com>
2014-09-16 09:41:30 -0600

    Implement 'Idea' with sorting

--- test/ideabox/idea_test.rb
+++ test/ideabox/idea_test.rb
@@ -15,4 +15,27 @@ class IdeaTest < Minitest::Test
     idea.like!
     assert_equal 1, idea.likes
   end
+
+  def test_ideas_can_be_liked_more_than_once
+    idea = Idea.new("diet", "carrots and cucumbers")
+    assert_equal 0, idea.likes
+    idea.like!
+    assert_equal 1, idea.likes
+    idea.like!
+    assert_equal 2, idea.likes
+  end
+
+  def test_ideas_can_be_sorted_by_rank
+    diet = Idea.new("diet", "cabbage soup")
+    exercise = Idea.new("exercise", "long distance running")
+    drink = Idea.new("drink", "carrot smoothy")
+
+    exercise.like!
+    exercise.like!
+    drink.like!
+
+    ideas = [drink, exercise, diet]
+
+    assert_equal [diet, drink, exercise], ideas.sort
+  end
 end
